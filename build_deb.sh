35a45a6f3f4e6fcefe3a8ed07ab35e5848571a16
riban <brian@riban.co.uk>
2020-10-28 11:12:04 +0000

    Adds deb packaging

---
+++ build_deb.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Script to build .deb package
+
+if [ $# -eq 0 ]
+then
+  echo Usage: build_deb.sh package_name
+  exit 1
+fi
+
+mkdir -p "deb/usr/bin"
+mkdir -p "deb/usr/share/applications"
+mkdir -p "deb/usr/share/doc/$1"
+mkdir -p "deb/usr/share/man/man1"
+
+cp -r DEBIAN deb
+cp "$1" deb/usr/bin
+gzip -n --best -c changelog > "deb/usr/share/doc/$1/changelog.gz"
+gzip -n --best -c $1.man > "deb/usr/share/man/man1/$1.1.gz"
+cp copyright "deb/usr/share.doc/"
+cp desktop "deb/usr/share/applications/$1/$1.desktop"
+
+dpkg-deb --build 
