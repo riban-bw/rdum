4c0b83277f0dd771b55f3593f26504df6b45ee9e
riban <brian@riban.co.uk>
2020-10-28 11:53:28 +0000

    Fixes debian package build script. Fixes desktop config. Refreshes debian package.

--- build_deb.sh
+++ build_deb.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 # Script to build .deb package
 
-if [ $# -eq 0 ]
+PACKAGE=rdum
+
+if [ $# -gt 0 ]
 then
-  echo Usage: build_deb.sh package_name
-  exit 1
+  PACKAGE=$1
 fi
 
 rm -r deb
 mkdir -p "deb/usr/bin"
 mkdir -p "deb/usr/share/applications"
-mkdir -p "deb/usr/share/doc/$1"
+mkdir -p "deb/usr/share/doc/$PACKAGE"
 mkdir -p "deb/usr/share/man/man1"
 
 chmod -R 0755 deb
 
 cp -r DEBIAN deb
-cp "$1" deb/usr/bin
-gzip -n --best -c changelog > "deb/usr/share/doc/$1/changelog.gz"
-chmod 0644 "deb/usr/share/doc/$1/changelog.gz"
-gzip -n --best -c $1.man > "deb/usr/share/man/man1/$1.1.gz"
-chmod 0644 "deb/usr/share/man/man1/$1.1.gz"
-cp copyright "deb/usr/share/doc/$1/"
-cp desktop "deb/usr/share/applications/$1/$1.desktop"
-chmod 
-fakeroot dpkg-deb --build deb $1.deb && lintian $1.deb && rm -r deb
+cp "$PACKAGE" deb/usr/bin
+gzip -n --best -c changelog > "deb/usr/share/doc/$PACKAGE/changelog.gz"
+chmod 0644 "deb/usr/share/doc/$PACKAGE/changelog.gz"
+gzip -n --best -c $PACKAGE.man > "deb/usr/share/man/man1/$PACKAGE.1.gz"
+chmod 0644 "deb/usr/share/man/man1/$PACKAGE.1.gz"
+cp copyright "deb/usr/share/doc/$PACKAGE/"
+cp desktop "deb/usr/share/applications/$PACKAGE.desktop"
+fakeroot dpkg-deb --build deb $PACKAGE.deb && lintian $PACKAGE.deb && rm -r deb
