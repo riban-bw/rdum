4adee8d92d36e2ec3bb887d1e5cfa73a5b60f27e
riban <brian@riban.co.uk>
2020-10-28 11:20:45 +0000

    Fixes deb package build script. Adds current deb package.

--- build_deb.sh
+++ build_deb.sh
@@ -7,16 +7,21 @@ then
   exit 1
 fi
 
+rm -r deb
 mkdir -p "deb/usr/bin"
 mkdir -p "deb/usr/share/applications"
 mkdir -p "deb/usr/share/doc/$1"
 mkdir -p "deb/usr/share/man/man1"
 
+chmod -R 0755 deb
+
 cp -r DEBIAN deb
 cp "$1" deb/usr/bin
 gzip -n --best -c changelog > "deb/usr/share/doc/$1/changelog.gz"
+chmod 0644 "deb/usr/share/doc/$1/changelog.gz"
 gzip -n --best -c $1.man > "deb/usr/share/man/man1/$1.1.gz"
-cp copyright "deb/usr/share.doc/"
+chmod 0644 "deb/usr/share/man/man1/$1.1.gz"
+cp copyright "deb/usr/share/doc/$1/"
 cp desktop "deb/usr/share/applications/$1/$1.desktop"
-
-dpkg-deb --build 
+chmod 
+fakeroot dpkg-deb --build deb $1.deb && lintian $1.deb && rm -r deb
