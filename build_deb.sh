cbfcd87f13e0954b0f7d2d41b1f016f452c092c4
brian <brian@riban.co.uk>
2020-11-25 09:55:35 +0000

    Moved deb package to x11 section. Fixes deb package file permissions. Add feature to hide if no updates available. Stop any currently running instances. Bumps minor version.

--- build_deb.sh
+++ build_deb.sh
@@ -18,10 +18,13 @@ chmod -R 0755 deb
 
 cp -r DEBIAN deb
 cp "$PACKAGE" deb/usr/bin
+chmod 755 "deb/usr/bin/$PACKAGE"
 gzip -n --best -c changelog > "deb/usr/share/doc/$PACKAGE/changelog.gz"
 chmod 0644 "deb/usr/share/doc/$PACKAGE/changelog.gz"
 gzip -n --best -c $PACKAGE.man > "deb/usr/share/man/man1/$PACKAGE.1.gz"
 chmod 0644 "deb/usr/share/man/man1/$PACKAGE.1.gz"
 cp copyright "deb/usr/share/doc/$PACKAGE/"
+chmod 0644 "deb/usr/share/doc/$PACKAGE/copyright"
 cp desktop "deb/usr/share/applications/$PACKAGE.desktop"
+chmod 0644 "deb/usr/share/applications/$PACKAGE.desktop"
 fakeroot dpkg-deb --build deb $PACKAGE.deb && lintian $PACKAGE.deb && rm -r deb
